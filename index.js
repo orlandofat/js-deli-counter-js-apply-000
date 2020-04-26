abe6826d669d5449b8c6593c984850af26ab8e27
Orlando Challenger <orlandofat@gmail.com>
2020-04-26 02:22:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ function currentLine (katzDeliLine){
   if(katzDeliLine.length === 0){
     return 'The line is currently empty.';
   }
-  
     for(var i = 0; i < katzDeliLine.length; i++){
       katzDeliLine = line.push(`${i+1}. ${katzDeliLine[i]}`);
       
